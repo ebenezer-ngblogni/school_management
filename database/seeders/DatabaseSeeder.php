b1172687098650787c852898ca8d14d142f7db54
ebenezer-ngblogni <ebengamer6@gmail.com>
2025-02-11 17:32:31 +0100

    reconfiguration du databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(DemoAccountsSeeder::class);
         $this->call(DemoUsersSeeder::class);
         $this->call(WeekSeeder::class);
 
